03441d91c347930d483c281b89fcbccb55bf9865
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2021-11-16 11:52:49 -0500

    Implement write()

--- src/extension.ts
+++ src/extension.ts
@@ -32,7 +32,22 @@ function transform(code: string): string {
 }
 
 function write(code: string): void {
-  // TODO: implement
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    throw new Error("There's no active editor");
+  }
+
+  const edit = new vscode.WorkspaceEdit();
+
+  const wholeDocument = new vscode.Range(
+    new vscode.Position(0, 0),
+    new vscode.Position(editor.document.lineCount, 0)
+  );
+  const updateCode = new vscode.TextEdit(wholeDocument, code);
+
+  edit.set(editor.document.uri, [updateCode]);
+
+  vscode.workspace.applyEdit(edit);
 }
 
 export function deactivate() {}
