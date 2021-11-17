e5650a86308d5adce77e32a073ea5b96b026c2c1
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2021-11-17 11:22:26 -0500

    Comment out tests we won't run
    We have decided to start testing the transform() method, so we don't have
    to bother with the VS Code API. This will make our tests faster, but leave
    some blind spots: read() and write()
    
    That's fine because:
    1. We manually tested these
    2. They won't change anymore

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,17 @@
-import * as assert from "assert";
+// (!) Skip it for now: test transformation code first, VS Code API later
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-import * as vscode from "vscode";
-// import * as myExtension from '../../extension';
+// import * as assert from "assert";
 
-suite("Extension Test Suite", () => {
-  vscode.window.showInformationMessage("Start all tests.");
+// // You can import and use all API from the 'vscode' module
+// // as well as import your extension to test it
+// import * as vscode from "vscode";
+// // import * as myExtension from '../../extension';
 
-  test("Sample test", () => {
-    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-  });
-});
+// suite("Extension Test Suite", () => {
+//   vscode.window.showInformationMessage("Start all tests.");
+
+//   test("Sample test", () => {
+//     assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+//     assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+//   });
+// });
