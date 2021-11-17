6dedc1d27869d25bd04382183ffc61f0f0357696
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2021-11-17 11:49:41 -0500

    Implement first scenario (unwrap alternate)

--- src/transform.ts
+++ src/transform.ts
@@ -1,28 +1,26 @@
 import generate from "@babel/generator";
 import { parse } from "@babel/parser";
-import traverse from "@babel/traverse";
-import { identifier } from "@babel/types";
+import traverse, { NodePath } from "@babel/traverse";
+import { IfStatement, isBlockStatement } from "@babel/types";
 
 export function transform(code: string): string {
   const ast = parse(code);
+  // 💡 Use AST Explorer to discover the AST structure:
+  // https://astexplorer.net/
   traverse(ast, {
-    Identifier(path) {
-      // 💡 Use AST Explorer to discover the AST structure:
-      // https://astexplorer.net/
-
-      if (path.node.name === "result") {
-        const newIdentifier = identifier("response");
-        path.replaceWith(newIdentifier);
-      } else {
-        path.node.name = capitalize(path.node.name);
-      }
+    IfStatement(path) {
+      unwrapAlternate(path);
     },
   });
   return generate(ast).code;
 }
 
-function capitalize(name: string): string {
-  const firstLetter = name[0];
-  const restOfName = name.slice(1);
-  return `${firstLetter.toUpperCase()}${restOfName}`;
+function unwrapAlternate(path: NodePath<IfStatement>) {
+  if (!isBlockStatement(path.node.alternate)) {
+    return;
+  }
+
+  const alternateStatements = path.node.alternate.body;
+  path.insertAfter(alternateStatements);
+  path.node.alternate = null;
 }
