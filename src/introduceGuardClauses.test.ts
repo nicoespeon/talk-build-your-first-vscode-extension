75dc400750eac6b103801cf53cda7cead5ed6832
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2021-11-17 12:23:39 -0500

    Add TODO exercise for reader

--- src/introduceGuardClauses.test.ts
+++ src/introduceGuardClauses.test.ts
@@ -83,4 +83,50 @@ function simpleScenario() {
 }`
     );
   });
+
+  // TODO: exercise for reader => implement this behavior
+  it.skip("introduce guard clauses across code", () => {
+    const code = `
+function getPayAmount() {
+  let result;
+
+  if (isDead) {
+    result = deadAmount();
+  } else {
+    if (isSeparated) {
+      result = separatedAmount();
+    } else {
+      if (isRetired) {
+        result = retiredAmount();
+      } else {
+        result = normalPayAmount();
+      }
+    }
+  }
+
+  return result;
+}`;
+
+    const result = transform(code);
+
+    assertAreEqual(
+      result,
+      `
+function getPayAmount() {
+  if (isDead) {
+    return deadAmount();
+  }
+
+  if (isSeparated) {
+    return separatedAmount();
+  }
+
+  if (isRetired) {
+    return retiredAmount();
+  }
+
+  return normalPayAmount();
+}`
+    );
+  });
 });
