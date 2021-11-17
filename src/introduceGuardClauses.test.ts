667afd384c6dc3b801fbfa79df1a0ecc84d0f8bc
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2021-11-17 11:40:27 -0500

    Add test for a basic valuable scenario

--- src/introduceGuardClauses.test.ts
+++ src/introduceGuardClauses.test.ts
@@ -1,7 +1,42 @@
 import assert = require("assert");
+import { transform } from "./transform";
 
 describe("Introduce Guard Clauses", () => {
-  it("should run tests", () => {
-    assert(false, "oh no, it failed");
+  it("remove the redundant else block", () => {
+    const code = `
+function simpleScenario() {
+  if (isAlive) {
+    return true;
+  } else {
+    doSomething();
+    return false;
+  }
+}`;
+
+    const result = transform(code);
+
+    const expectedCode = `
+function simpleScenario() {
+  if (isAlive) {
+    return true;
+  }
+
+  doSomething();
+  return false;
+}`;
+    assert(
+      result.trim() === expectedCode.trim(),
+      `
+Expected:
+
+${expectedCode}
+
+---
+
+Actual:
+
+${result}
+`
+    );
   });
 });
